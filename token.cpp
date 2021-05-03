c28a89c178b07cd88d0358fa549c9c8f79c92962
Mohamad assi <mhmdassi@hotmail.com>
2021-05-03 07:54:40 +0300

    fixed merge

--- token.cpp
+++ token.cpp
@@ -2,33 +2,16 @@
 #include "token.h"
 #include "tokenstrings.h"
 
-tokenType Token::getType(){
-    return type;
-}
+tokenType Token::getType(){return type;}
+string Token::getText(){return text;}
 
 void Token::printToken(){
-<<<<<<< HEAD
-    string tokenTemp = "";
     string token = text;
-
     if(type >= IDENTIFIER){
-        tokenTemp += token;
         token = tokenStrings[type - IDENTIFIER];
     }
-//    cout << tokenTemp << endl;
-    cout << token << "\t" << text << endl;
-=======
-    string temp_text = text;
-    if(type >= IDENTIFIER){
-        text = tokenStrings[type - IDENTIFIER];
-    }
-    cout << text << "\t" << text << endl;
-    text = temp_text;
-}
 
-string Token::getText(){
-    return text;
->>>>>>> 5d147cdf70ed010d525e88e7903b6d3df8155128
+    cout << token << "\t" << text << endl;
 }
 
 void varToken::add_line(int l){
